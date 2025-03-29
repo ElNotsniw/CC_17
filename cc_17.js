d72fc9d682b6e8a12b4044c9e3d6f95ee4062bff
ElNotsniw <winstonjosh010604@gmail.com>
2025-03-29 16:16:30 -0400

    Task 4 - Finalized reporting system

--- cc_17.js
+++ cc_17.js
@@ -121,3 +121,62 @@ vipcustomer.addPurchase(50);
 console.log(`VIP Customer: ${vipcustomer.name}`);
 console.log(`VIP Level: ${vipcustomer.viplevel}`);
 console.log(`Total amount spent with bonus: $${vipcustomer.gettotalspent().toFixed(2)}`);
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+
+// Task 4 - Build a Client Report System
+
+// Created one more customer to have a total of 4 customers with both regular and VIP status.
+
+const vipcustomer2 = new VIPCustomer("Arik Vahid", "arikvahid@gmail.com", "Platinum")
+
+// Recalling some of my customers from my previous tasks and adding the new customers with their spending amount.
+
+customer1.addPurchase(50);
+customer1.addPurchase(150);
+
+customer2.addPurchase(40);
+customer2.addPurchase(100);
+
+vipcustomer.addPurchase(300);
+vipcustomer.addPurchase(500);
+
+vipcustomer2.addPurchase(400);
+vipcustomer2.addPurchase(600);
+
+
+salesRep.addClient(customer1);
+salesRep.addClient(customer2);
+salesRep.addClient(vipcustomer);
+salesRep.addClient(vipcustomer2);
+
+
+// Calculating the total revenue from all the customers' spending and also filtering customers who have spent more than $500 in their own category.
+
+const totalrevenue = salesRep.clients.reduce((total, client) => total+client.gettotalspent(), 0)
+
+const highspendingcustomer = salesRep.clients.filter(client => client.gettotalspent() > 500);
+
+// Creating a function to show each customer's name and their spending amount.
+
+const customersummary = salesRep.clients.map(client => ({
+    name: client.name,
+    totalspent: client.gettotalspent()
+}));
+
+
+// Finally, console-logging the total revenue, filteration of high-spending and low-spending customer, and the customer summary.
+
+console.log(`Total Revenue: $${totalrevenue.toFixed(2)}`);
+
+console.log("High-Spending Customers:");
+highspendingcustomer.forEach(client => {
+    console.log(`${client.name}: $${client.gettotalspent().toFixed(2)}`);
+});
+
+console.log('Customer Summary:');
+customersummary.forEach(summary => {
+    console.log(`${summary.name} spent $${summary.totalspent.toFixed(2)}`);
+});
